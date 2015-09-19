277a4bf5310b31e7d2fbfb50bab3132f53b3631c
Bernard <bernardhamann@users.noreply.github.com>
2015-09-19 05:25:57 +0200

    Delete config.js

--- arc/config.js
+++
@@ -1,9 +0,0 @@
-var config = {};
-
-config.siteName = 'The Name of your site';
-config.rootURL = "http://localhost:3001";
-config.PORT = 3001;
-config.MONGO_URL = 'mongodb://name:password@server.com:10234/appnamedb';
-config.SENDGRID_APIKEY = '34r245t45t453435345435';
-
-module.exports = config;
