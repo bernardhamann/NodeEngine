bce26b46b9a68c4848e12f373b9e20954327e598
bernardhamann <info@node-engine.com>
2015-10-10 20:27:39 +0200

    =>

--- node_engine/ne-gulp/appmeta.js
+++ node_engine/ne-gulp/appmeta.js
@@ -25,7 +25,6 @@ var appmeta = [
   description: "This is People page",
   neDataBefore: 1,
   nedb1: {
-    path: "http://localhost:3001/api/people",
     pathFunction: function pathFunction(meta) {
             if (meta.params._id) {
                 path = process.env.ROOTURL + "/api/people" + "/" + meta.params._id + "?token=" + meta.token;
@@ -49,6 +48,58 @@ var appmeta = [
   path: "/posts/:id",
   title: "About Us",
   description: "This is About Us page"
+},{
+  path: "/super",
+  title: "Super Admin Page",
+  description: "Use the super admin key to create admin tokens and assign them to users",
+  neDataBefore: 2,
+  nedb1: {
+    pathFunction: function pathFunction(meta) {
+
+            console.log(meta);
+
+            if (meta.query) {
+                if (meta.query.super_token) {
+                    var path = process.env.ROOTURL + "/admin/api/tokens/admin" + "?access_token=" + meta.query.super_token;
+                } else {
+                    var path = process.env.ROOTURL + "/admin/api/tokens/admin";
+                }
+            } else {
+                var path = process.env.ROOTURL + "/admin/api/tokens/admin";
+            }
+
+            return path;
+        }
+  },
+  nedb2: {
+    pathFunction: function pathFunction(meta) {
+
+            console.log(meta);
+
+            if (meta.query) {
+                if (meta.query.admin_token) {
+                    var path = process.env.ROOTURL + "/admin/api/users" + "?access_token=" + meta.query.admin_token;
+                } else {
+                    var path = process.env.ROOTURL + "/admin/api/users";
+                }
+            } else {
+                var path = process.env.ROOTURL + "/admin/api/tokens/admin";
+            }
+
+            return path;
+        }
+  }
+},{
+  path: "/admin/users/:_id",
+  title: "Edit User",
+  description: "Editing user",
+  neDataBefore: 1,
+  nedb1: {
+    pathFunction: function pathFunction(meta) {
+            path = process.env.ROOTURL + "/api/users/" + meta.params._id + "?token=" + meta.token;
+            return path;
+        }
+  }
 },{
   path: "/login",
   title: "Login",
