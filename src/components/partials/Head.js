0e7ff27aadcded42c725bbd129115d5b8d5cac9c
bernardhamann <info@node-engine.com>
2015-10-16 01:50:54 +0200

    allow for custom css

--- src/components/partials/Head.js
+++ src/components/partials/Head.js
@@ -4,11 +4,23 @@ class Head extends React.Component {
 
     render(){
 
+        if (this.props.meta && this.props.meta.css){
+            var css = this.props.meta.css.map(function(sheet, index){
+                return <link rel="stylesheet" type="text/css" href={sheet}/>
+            });
+        }
+        else {
+            var css = null
+        }
+
+
+
         return (
             <head>
                 <title>{`${this.props.meta.title} - ${this.props.meta.appname}`}</title>
                 <meta name="description" content={this.props.meta.description}/>
                 <link rel='stylesheet' href='/style.css' />
+                {css}
                 <meta charSet="UTF-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
